8df883a6ffffb78615929ed351b68bc4213cafe8
felipecavassana <web4406@caelum5713-Macmini>
2014-07-05 15:40:30 -0300

    Aula 05072014

--- src/br/com/caelum/jdbc/teste/TestaInsere.java
+++ src/br/com/caelum/jdbc/teste/TestaInsere.java
@@ -9,7 +9,7 @@ import br.com.caelum.jdbc.modelo.Contato;
 public class TestaInsere {
 	public static void main(String[] args) throws SQLException{
 		// pronto para gravar
-		Contato contato = new Contato();
+		Contato contato = new Contato(); 
 		contato.setNome("Caelum");
 		contato.setEmail("contato@caelum.com.br");
 		contato.setEndereco("R. Vergueiro 3185 cj57");
